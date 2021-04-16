3713356715554c7cdd168a68ac84cca4b80114e5
Francisco <franciscoparisalbero@gmail.com>
2021-04-16 12:15:13 +0200

    Updates

---
+++ precache-manifest.ff76134fe8bd728b7529aa518b556aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c5f31b06b989fba12d1a87e31326a8",
+    "url": "/web_tnk_hospitales/static/media/logo_copy.14c5f31b.png"
+  },
+  {
+    "revision": "930d7ad5debd1aa9462e",
+    "url": "/web_tnk_hospitales/static/js/runtime~main.f3970cd3.js"
+  },
+  {
+    "revision": "1185bd78ad4483b58154",
+    "url": "/web_tnk_hospitales/static/js/main.d9a6de93.chunk.js"
+  },
+  {
+    "revision": "7d1b44e927343c355fd1",
+    "url": "/web_tnk_hospitales/static/js/2.a7af2a1d.chunk.js"
+  },
+  {
+    "revision": "1185bd78ad4483b58154",
+    "url": "/web_tnk_hospitales/static/css/main.eee48285.chunk.css"
+  },
+  {
+    "revision": "31ced2a42c5396e9c90e06d1be4eee87",
+    "url": "/web_tnk_hospitales/index.html"
+  }
+];
